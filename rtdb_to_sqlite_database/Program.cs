418fa544362dc59da7a1f8eb5ea5ba26a018b911
Sergey <enclave2005@gmail.com>
2023-04-05 22:20:09 +0500

    Add project files.

---
+++ rtdb_to_sqlite_database/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rtdb_to_sqlite_database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
